d2e4d803136600604ad84de7316b51035aa82798
Eric Thomas <ethomasosu@gmail.com>
2022-12-20 14:23:18 -0500

    linting

--- index.js
+++ index.js
@@ -3,19 +3,21 @@ const scrollableTextBox = document.getElementById("scrollableTextBox");
 var submissions = []
 
 filterSearch.addEventListener("keyup", function() {
-  const filter = this.value.toLowerCase();
-  console.log(filter)
-  const filteredData = submissions.filter(item => item.toLowerCase().startsWith(filter));
-  scrollableTextBox.value = filteredData.join("\n");
+    const filter = this.value.toLowerCase();
+    console.log(filter)
+    const filteredData = submissions.filter(item => item.toLowerCase().startsWith(filter));
+    scrollableTextBox.value = filteredData.join("\n");
 });
 
 fetch("https://brscvg5a2a.execute-api.us-east-1.amazonaws.com")
-  .then(response => response.json())
-  .then(rounds => {
-    for (const [roundName, results] of Object.entries(rounds)) {
-        for (var i = 0; i < results.length; i++){
+    .then(response => response.json())
+    .then(response => populate_search_box(response));
+
+function populate_search_box(response) {
+    for (const [roundName, results] of Object.entries(response)) {
+        for (var i = 0; i < results.length; i++) {
             submissions.push(`${results[i]["song"]}  - ${results[i]["artist"]} (Submitted by ${results[i]["submitter_name"]} ${results[i]["number_of_votes"]} votes)`)
         }
     }
     scrollableTextBox.value = submissions.join("\n");
-  });
+}
